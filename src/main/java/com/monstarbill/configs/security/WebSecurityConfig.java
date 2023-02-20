13bafa3c5e5581c6b1cf1dbb453f4ab154234065
vidhateprashant <vidhate.prashant98@gmail.com>
2023-02-20 13:13:35 +0530

    cors

--- src/main/java/com/monstarbill/configs/security/WebSecurityConfig.java
+++ src/main/java/com/monstarbill/configs/security/WebSecurityConfig.java
@@ -17,11 +17,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.monstarbill.configs.security.jwt.AuthEntryPointJwt;
 import com.monstarbill.configs.security.jwt.AuthTokenFilter;
 import com.monstarbill.configs.service.impl.UserDetailsServiceImpl;
-
 
 @Configuration
 @EnableWebSecurity
@@ -61,25 +61,45 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors();
 		http.csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
-				.antMatchers("/**").permitAll()
-				.antMatchers("/api/auth/**").permitAll().antMatchers("/api/test/admin**").hasAnyRole("ADMIN")
-				.antMatchers("/api/test/mod**", "/api/v1/case/get-cases**").hasAnyRole("ADMIN", "MODERATOR")
-				.antMatchers("/api/test/user**").hasAnyRole("ADMIN", "MODERATOR", "USER").anyRequest().permitAll()
-				.and().formLogin().permitAll()
-				.and().logout();
+				.antMatchers("/**").permitAll().antMatchers("/api/auth/**").permitAll().antMatchers("/api/test/admin**")
+				.hasAnyRole("ADMIN").antMatchers("/api/test/mod**", "/api/v1/case/get-cases**")
+				.hasAnyRole("ADMIN", "MODERATOR").antMatchers("/api/test/user**")
+				.hasAnyRole("ADMIN", "MODERATOR", "USER").anyRequest().permitAll().and().formLogin().permitAll().and()
+				.logout();
 
+		http.addFilterBefore(corsFilter(), CorsFilter.class);
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+		
 	}
-	
+
 	@Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setExposedHeaders(Arrays.asList("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		configuration.setExposedHeaders(Arrays.asList("*"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		// config.setAllowCredentials(true); // you USUALLY want this
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 }
